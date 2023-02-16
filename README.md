2b5d0b2cc6f7e08de649b4b5b3dc2196a6c50d7c
Quinn Henry <78760148+TheQuib@users.noreply.github.com>
2023-02-16 17:18:23 -0500

    💨 Add initial files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # python-whoopee-cushion
 Python flask web server with a button a few sounds to play fart noises from the server.
+ - This project is meant for a Raspberry Pi, but most Linux distributions will work
+ - This was mostly written by ChatGPT
+
+# Get started
+
+## Install Python
+You can get Python from the [official website](https://www.python.org/downloads/)
+
+## Install Python dependencies
+Once Python is installed, install necessary dependencies (`flask` and `pygame`) with:
+```bash
+py -m pip install flask pygame
+```
+
+## Run the web server
+Run the web server with `py main.py`
+
+
+# Attributions
+
+## Audio files
+Fart audio files from the Raspberry Pi foundation's [Whoopi cushion project](https://projects.raspberrypi.org/en/projects/whoopi-cushion)
---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect
+import random
+import pygame
+import os
+
+app = Flask(__name__)
+
+# Set '__location__' variable to directory of script
+__location__ = os.path.realpath(
+    os.path.join(
+        os.getcwd(),
+        os.path.dirname(
+            __file__
+        )
+    )
+)
+
+# Set variables to the script location + fileName
+fartFile1 = __location__ + '\\assets\\audio\\fart1.wav'
+fartFile2 = __location__ + '\\assets\\audio\\fart2.wav'
+fartFile3 = __location__ + '\\assets\\audio\\fart3.wav'
+
+
+# Initialize pygame mixer
+pygame.mixer.init()
+
+# Define route for index page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Define route for playing the fart sound
+@app.route('/fart')
+def fart():
+    # Load and play the fart sound
+    #fart_sounds = [fartFile1, fartFile2, fartFile3]
+    fart_sounds = [fartFile1, fartFile2, fartFile3]
+    random_fart_sound = random.choice(fart_sounds)
+    fart_sound = pygame.mixer.Sound(random_fart_sound)
+    fart_sound.play()
+    # Redirect back to the root directory
+    return redirect("/")
+
+# Run the app
+app.run(host='0.0.0.0', port=5000, debug=False)
